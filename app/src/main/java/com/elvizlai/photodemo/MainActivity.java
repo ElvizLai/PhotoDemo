b3640ec3cbb8ff19044af47e06b6134ba56f1be4
ElvizLai <sdrzlyz@126.com>
2014-11-13 19:38:47 +0800

    init

---
+++ app/src/main/java/com/elvizlai/photodemo/MainActivity.java
@@ -0,0 +1,93 @@
+package com.elvizlai.photodemo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewTreeObserver;
+import android.widget.GridView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    /**
+     * 用于展示照片墙的GridView
+     */
+    private GridView mPhotoWall;
+
+    /**
+     * GridView的适配器
+     */
+    private PhotoWallAdapter mAdapter;
+
+    private int mImageThumbSize;
+    private int mImageThumbSpacing;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageThumbSize = getResources().getDimensionPixelSize(
+                R.dimen.image_thumbnail_size);
+        mImageThumbSpacing = getResources().getDimensionPixelSize(
+                R.dimen.image_thumbnail_spacing);
+        mPhotoWall = (GridView) findViewById(R.id.photo_wall);
+        mAdapter = new PhotoWallAdapter(this, 0, Images.imageThumbUrls,
+                mPhotoWall);
+        mPhotoWall.setAdapter(mAdapter);
+        mPhotoWall.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+
+                    @Override
+                    public void onGlobalLayout() {
+                        final int numColumns = (int) Math.floor(mPhotoWall
+                                .getWidth()
+                                / (mImageThumbSize + mImageThumbSpacing));
+                        if (numColumns > 0) {
+                            int columnWidth = (mPhotoWall.getWidth() / numColumns)
+                                    - mImageThumbSpacing;
+                            mAdapter.setItemHeight(columnWidth);
+                            mPhotoWall.getViewTreeObserver()
+                                    .removeGlobalOnLayoutListener(this);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAdapter.fluchCache();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 退出程序时结束所有的下载任务
+        mAdapter.cancelAllTasks();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
